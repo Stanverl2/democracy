a983f80e7821dfec67f4721f700f65bd4420e40d
Stan Verlaan <s.verlaan@student.tudelft.nl>
2025-12-15 13:56:38 +0100

    Changed the community to send a default election message every 5 seconds

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from asyncio import run
 
-from communities.MyCommunity import MyCommunity
+from communities.ElectionCommunity import ElectionCommunity
 
 from ipv8.configuration import ConfigBuilder, Strategy, WalkerDefinition, default_bootstrap_defs
 from ipv8_service import IPv8
@@ -13,12 +13,12 @@ async def start_communities(n: int) -> None:
         builder = ConfigBuilder().clear_keys().clear_overlays()
         os.makedirs("keys", exist_ok=True)
         builder.add_key("my peer", "medium", f"keys/ec{i}.pem")
-        builder.add_overlay("MyCommunity", "my peer",
+        builder.add_overlay("ElectionCommunity", "my peer",
                             [WalkerDefinition(Strategy.RandomWalk,
                                               10, {"timeout": 3.0})],
                             default_bootstrap_defs, {}, [("started",)])
         await IPv8(builder.finalize(),
-                   extra_communities={"MyCommunity": MyCommunity}).start()
+                   extra_communities={"ElectionCommunity": ElectionCommunity}).start()
     await run_forever()
 
 
