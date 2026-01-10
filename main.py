5dbcec746ee187eb82212321a9dfa3a6f7f2db1a
Stan Verlaan <s.verlaan@student.tudelft.nl>
2026-01-10 13:04:50 +0100

    First implementation of a frontend that allows for creating elections, viewing elections, and voting on elections

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from asyncio import run
+import os
 
-from communities.MyCommunity import MyCommunity
-
-from ipv8.configuration import ConfigBuilder, Strategy, WalkerDefinition, default_bootstrap_defs
+from ipv8.configuration import ConfigBuilder, default_bootstrap_defs, Strategy, WalkerDefinition
 from ipv8_service import IPv8
 from ipv8.util import run_forever
+from pathlib import Path
 
-import os
+from communities.MyCommunity import MyCommunity
+from config import DATA_PATH
+from models.election import Election
+from models.vote import Vote
+from storage.json_store import JSONStore
+from ui.app import Application
+
+def main():
+    election_store = JSONStore[Election](
+        path=Path(DATA_PATH + "elections.json"),
+        model_factory=Election.from_dict,
+        dictify=lambda e: e.to_dict()
+    )
+    vote_store = JSONStore[Vote](
+        path=Path(DATA_PATH + "votes.json"),
+        model_factory=Vote.from_dict,
+        dictify=lambda v: v.to_dict()
+    )
+    app = Application(election_store, vote_store)
+    app.run()
 
 async def start_communities(n: int) -> None:
     for i in range(0, n):
@@ -21,5 +39,5 @@ async def start_communities(n: int) -> None:
                    extra_communities={"MyCommunity": MyCommunity}).start()
     await run_forever()
 
-
-run(start_communities(3))
+if __name__ == "__main__":
+    main()
