d421075512c583de41042cb67ed26df3c96bcffd
Stan Verlaan <s.verlaan@student.tudelft.nl>
2026-02-16 17:14:58 +0100

    Added sharing of votes between instances upon voting and periodically

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ from typing import Final
 
 DATA_PATH: Final[str] = "data/"
 COMMUNITY_ID: Final[bytes] = hashlib.sha1(b"ElectionCommunity").digest()
+COMMUNICATION_INTERVAL: Final[float] = 60.0 # Seconds
